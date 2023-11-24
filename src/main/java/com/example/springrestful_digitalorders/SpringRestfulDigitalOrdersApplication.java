323d4b9606706d415e3b7bc343c7646459c0b0da
nadir inab <nadir.inab.dev@gmail.com>
2023-11-24 11:45:24 +0100

    first commit project is well initiliazed

---
+++ src/main/java/com/example/springrestful_digitalorders/SpringRestfulDigitalOrdersApplication.java
@@ -0,0 +1,14 @@
+package com.example.springrestful_digitalorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulDigitalOrdersApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Earth !!");
+		SpringApplication.run(SpringRestfulDigitalOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestful_digitalorders/SpringRestfulDigitalOrdersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springrestful_digitalorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringRestfulDigitalOrdersApplicationTests {
+
+	public static void main(String[] args) {
+		//System.out.println("Hllo earth");
+		SpringApplication.run(SpringRestfulDigitalOrdersApplication.class, args);
+	}
+
+
+}
