ed1c01bc0933de7678bcf5c7678e1de16290889d
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2023-11-27 11:18:10 +0100

    Add Service and Repository logic

--- src/main/java/com/example/springrestful_digitalorders/application/contract/ContractServiceImpl.java
+++ src/main/java/com/example/springrestful_digitalorders/application/contract/ContractServiceImpl.java
@@ -1,4 +1,45 @@
 package com.example.springrestful_digitalorders.application.contract;
 
-public class ContractServiceImpl {
+import com.example.springrestful_digitalorders.domain.contract.Contract;
+import com.example.springrestful_digitalorders.domain.contract.ContractRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ContractServiceImpl implements ContractService{
+    private final ContractRepository contractRepository;
+
+    @Override
+    public List<Contract> findAll() {
+        return contractRepository.findAll();
+    }
+
+    @Override
+    public Contract findById(Long id) {
+        return contractRepository.findById(id).orElseThrow(() -> new RuntimeException("Contract not found"));
+    }
+
+    @Override
+    public Contract save(Contract contract) {
+        return contractRepository.save(contract);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        contractRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Contract contract) {
+        contractRepository.delete(contract);
+    }
+
+    @Override
+    public Contract update(Contract contract) {
+        return contractRepository.save(contract);
+    }
 }
--- src/main/java/com/example/springrestful_digitalorders/domain/contract/Contract.java
+++ src/main/java/com/example/springrestful_digitalorders/domain/contract/Contract.java
@@ -1,4 +1,22 @@
 package com.example.springrestful_digitalorders.domain.contract;
 
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Contract {
+@Id @GeneratedValue
+private Long id;
+    @OneToOne
+    private Devis devis;
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
 }
+
